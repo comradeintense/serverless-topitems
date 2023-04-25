9e38ba02224ad30f70bc65f3e4466b7c7c03554d
comradeintense <in10se_SL@yahoo.com>
2023-04-25 16:20:41 +0300

    added cors

--- netlify/functions/itemdetails.js
+++ netlify/functions/itemdetails.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
 	try {
 		const fetch = (await import('node-fetch')).default;
 		const apiKey = process.env.STEAM_API_KEY || 'default_api_key';
-		const itemId = event.pathParameters.itemId;
+		const itemId = event.queryStringParameters.itemId;
 		const url = `https://api.steampowered.com/IPublishedFileService/GetDetails/v1/?key=${apiKey}&publishedfileids[0]=${itemId}&includevotes=true`;
 
 		const response = await fetch(url);
